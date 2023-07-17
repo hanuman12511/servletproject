e1d0982866bdd4e3adfb90b6d8e40ec03fe271e5
hanuman12511 <er.hanumansaini@gmail.com>
2023-07-17 21:25:22 +0530

    sasa

---
+++ WEB-INF/classes/servletclass/AddProduct.java
@@ -0,0 +1,44 @@
+package servletclass;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class AddProduct extends HttpServlet{
+  public void doPost(HttpServletRequest request,
+  HttpServletResponse response)
+  throws ServletException, IOException {
+  response.setContentType("text/html");
+  PrintWriter out = response.getWriter();
+  String name = request.getParameter("uname");
+  out.println("<html>");
+  out.println("<body>");
+  out.println("Thanks  Mr."+ name+"<br>");
+  out.println("</body></html>");
+  Connection connection=null;
+		try 
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/db","root","root");
+			PreparedStatement stmt;
+			String query="insert into  emp1(name) values(?)";
+			stmt=connection.prepareStatement(query);
+			stmt.setString(1,"name");
+			
+			
+			int row=stmt.executeUpdate(); // it returns no of rows affected.
+			
+			if(row>0)
+			{
+         out.println(" inser data");
+			}
+  }
+  catch(Exception e){
+    System.out.println(e);
+  }
+}
+}
---
+++ index.jsp
@@ -0,0 +1,11 @@
+<html>
+    <head></head>
+    <body>
+
+        <h1>programmin</h1>
+        <form action="/WebFirst/home" method="POST">
+            <input type="text" name="uname"/>
+            <input type="submit" value="submit"/>
+        </form>
+    </body>
+    </html>
